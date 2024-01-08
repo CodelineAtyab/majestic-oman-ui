d19b341c61d64757fdb01d1101892ebf8e504979
osamaline1229 <134750339+osamaline1229@users.noreply.github.com>
2024-01-08 13:30:16 +0300

    Ntra 104: fixed titles (#14)
    * the Website almost done
    
    * fixed titles
    
    * fixed titles

--- assets/vendor/cat-API/Cat.js
+++ assets/vendor/cat-API/Cat.js
@@ -1,35 +1,19 @@
-const url = "https://api.thecatapi.com/v1/images/search";
+const url = "http://omanmajesticapi.servepics.com:8080/api/v1/PicturesContent/random";
 const section = document.querySelector(".container");
 const button = document.querySelector(".btn");
 
 section.classList.add("cats");
 
-button.addEventListener("click", getRandomCats);
-
-randomCatPhoto = (json) => {
-  let photo = json[0].url;
-
+button.addEventListener("click", () => {
   let image = document.createElement("img");
   image.style.display="flex";
-  image.src = photo;
+  image.src = url;
   image.classList.add("random_cats");
-  image.alt = photo;
+  image.alt = url;
 
   image.style.border="3px solid black";
   image.style.borderRadius="20px 20px 20px 20px";
   image.style.boxShadow="20px 20px 5px #343a3f";
   section.appendChild(image);
-};
+});
 
-async function getRandomCats() {
-  section.innerHTML = "";
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log("JSON:", json);
-    return randomCatPhoto(json);
-  } catch (e) {
-    console.log("This is an error");
-    console.log(e);
-  }
-}
