de83654fca33cf84208fb330ee767143f4c837b3
CodelineAtyab <119934383+CodelineAtyab@users.noreply.github.com>
2024-01-09 11:04:34 +0400

    ntra-109: Supported Delete Functionality (#18)
    * delete functionality added
    
    * refactring
    
    * Corrected Delete Functionality
    
    ---------
    
    Co-authored-by: ahmedhh33 <ahmedhh3633@gmail.com>
    Co-authored-by: Syed Atyab Hussain <syed.atyab@codeline.rihal.om>

--- assets/js/admin-login-control/getAndDelete.js
+++ assets/js/admin-login-control/getAndDelete.js
@@ -1,30 +1,24 @@
-const hostName = window.location.hostname;
-const hostPort = 8080;
-const getSpecificPhotoBaseUrl = `http://omanmajesticapi.servepics.com:8080/api/v1/picturesContent`
+const getSpecificPhotoBaseUrl = `http://${serverAddress}:${serverPort}/api/v1/picturesContent`;
 const getAllPhotosJsonBaseUrl = `${getSpecificPhotoBaseUrl}/data`;
-
-// localStorage.getItem("LogInUsername");
-// localStorage.getItem("LogInPassword");
-
-
 
 /*
 Render existing Photo's list view.
 */
 const getAndRenderAllPhotoList = () => {
   fetch(`${getAllPhotosJsonBaseUrl}`)
-  .then((response) => {
-    return response.json();
-  })
-  .then((jsonResponse) => {
-    jsonResponse.forEach((currPicObj) => {
-      console.log(currPicObj.picID);
-      addPicRecordRowInDiv(`${getSpecificPhotoBaseUrl}/${currPicObj.picID}`);
+    .then((response) => {
+      return response.json();
     })
-  }).catch(error => {
-    console.error('Error:', error);
-    alert("Unable to get all of the photos.")
-  })
+    .then((jsonResponse) => {
+      jsonResponse.forEach((currPicObj) => {
+        console.log(currPicObj.picID);
+        addPicRecordRowInDiv(`${getSpecificPhotoBaseUrl}/${currPicObj.picID}`, currPicObj.picID);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Unable to get all of the photos.");
+    });
 };
 getAndRenderAllPhotoList();
 
@@ -39,27 +33,34 @@ document.getElementById("upload-form").addEventListener("submit", (event) => {
 
 const uploadSelectedPhoto = (fileToUpload) => {
   if (fileToUpload) {
-      const formData = new FormData();
-      formData.append('file', fileToUpload);
+    const formData = new FormData();
+    formData.append("file", fileToUpload);
 
-      var reqHeaders = new Headers();
-      reqHeaders.append("Authorization", `Basic ${btoa(`${localStorage.getItem("LogInUsername")}:${localStorage.getItem("LogInPassword")}`)}`);
+    var reqHeaders = new Headers();
+    reqHeaders.append(
+      "Authorization",
+      `Basic ${btoa(
+        `${localStorage.getItem("LogInUsername")}:${localStorage.getItem(
+          "LogInPassword"
+        )}`
+      )}`
+    );
 
-      fetch(getSpecificPhotoBaseUrl, {
-          method: 'POST',
-          headers: reqHeaders,
-          body: formData,
-      })
-      .then(response => response.text())
-      .then(data => {
+    fetch(getSpecificPhotoBaseUrl, {
+      method: "POST",
+      headers: reqHeaders,
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
         console.log(data);
-        
-        addPicRecordRowInDiv(`${getSpecificPhotoBaseUrl}/${data}`);
+
+        addPicRecordRowInDiv(`${getSpecificPhotoBaseUrl}/${data}`, data);
         alert("Photo Uploaded without a title and description.");
       })
-      .catch(error => {
-        console.error('Error:', error);
-        alert("Unable to upload the photo.")
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Unable to upload the photo.");
       });
   }
 };
@@ -67,14 +68,16 @@ const uploadSelectedPhoto = (fileToUpload) => {
 /*
 Utility functions.
 */
-const addPicRecordRowInDiv = (imageUrl) => {
+//added picid to pic the id of photo to delete it
+const addPicRecordRowInDiv = (imageUrl, picID) => {
+  // const imageUrl = `${getSpecificPhotoBaseUrl}/${currPicObj.picID}`
   const photoInfoContDiv = document.getElementById("photo_info_container_div");
   const outerContainerDiv = document.createElement("div");
   outerContainerDiv.className = "col-lg-6";
+  outerContainerDiv.id = picID;
 
   const innerContainerDiv = document.createElement("div");
-  innerContainerDiv.className =
-    "photo-item d-flex justify-content-between";
+  innerContainerDiv.className = "photo-item d-flex justify-content-between";
 
   const imgElem = document.createElement("img");
   imgElem.src = imageUrl;
@@ -86,19 +89,22 @@ const addPicRecordRowInDiv = (imageUrl) => {
   const actionContainerDiv = document.createElement("div");
   actionContainerDiv.className = "photo-actions";
 
-  const updateButton = document.createElement("button");
-  updateButton.className = "btn-update";
-  updateButton.onclick = () => {
-    console.log("Update Clicked!");
-  };
-  updateButton.innerText = "Update";
+  // const updateButton = document.createElement("button");
+  // updateButton.className = "btn-update";
+  // updateButton.onclick = () => {
+  //   console.log("Update Clicked!");
+  // };
+  // updateButton.innerText = "Update";
 
   const delButton = document.createElement("button");
   delButton.className = "btn-delete";
-  delButton.onclick = () => confirmDeletePhoto(currPhoto);
+
+  console.log(`Delete button has image id: ${picID}`);
+  delButton.onclick = () => confirmDeletePhoto(picID);
+  
   delButton.innerText = "Delete";
 
-  actionContainerDiv.appendChild(updateButton);
+  // actionContainerDiv.appendChild(updateButton);
   actionContainerDiv.appendChild(delButton);
 
   innerContainerDiv.appendChild(imgElem);
@@ -106,7 +112,7 @@ const addPicRecordRowInDiv = (imageUrl) => {
 
   outerContainerDiv.appendChild(innerContainerDiv);
   photoInfoContDiv.append(outerContainerDiv);
-}
+};
 
 // const respJson = ["profile-img.jpg", "apple-touch-icon.png"];
 
@@ -158,37 +164,46 @@ const addPicRecordRowInDiv = (imageUrl) => {
 //   console.log("HAHHAHHAHAHHAHHAHAHAHH I AM EXITING !!!!!");
 // });
 
-// Function to confirm deletion before making the API call
-// function confirmDeletePhoto(photoName) {
-//   const isConfirmed = window.confirm(
-//     `Are you sure you want to delete ${photoName}?`
-//   );
+//Function to confirm deletion before making the API call
+function confirmDeletePhoto(picId) {
+  const isConfirmed = window.confirm(
+    `Are you sure you want to delete ${picId}?`
+  );
 
-//   if (isConfirmed) {
-//     deletePhoto(photoName);
-//   }
-// }
+  if (isConfirmed) {
+    deletePhoto(picId);
+  }
+}
 
-// // Function to delete a photo
-// async function deletePhoto(photoName) {
-//   try {
-//     // Make an API call to delete the photo
-//     const response = await fetch(`/api/photos/${photoName}`, {
-//       method: "DELETE",
-//       headers: {
-//         "Content-Type": "application/json",
-//         // You may need to include additional headers or authentication tokens
-//       },
-//     });
+//Function to delete a photo
+const deletePhoto = (picId) => {
+  const myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    `Basic ${btoa(
+      `${localStorage.getItem("LogInUsername")}:${localStorage.getItem(
+        "LogInPassword"
+      )}`
+    )}`
+  );
+  // myHeaders.append("Cookie", "JSESSIONID=227D78B131038368298C107221758AAC");
 
-//     if (response.ok) {
-//       console.log(`Photo ${photoName} deleted successfully`);
-//       // Optionally, you can remove the deleted photo from the UI
-//       // e.g., outerContainerDiv.remove();
-//     } else {
-//       console.error(`Failed to delete photo ${photoName}`);
-//     }
-//   } catch (error) {
-//     console.error("Error deleting photo:", error);
-//   }
-// }
+  const requestOptions = {
+    method: "DELETE",
+    headers: myHeaders
+  };
+
+  fetch(`${getSpecificPhotoBaseUrl}/${picId}`, requestOptions)
+  .then(response => {
+    if (response.ok) {
+      console.log("Photo deleted successfully");
+      alert("Deleted Successfully!");
+      const photoInfoContDiv = document.getElementById("photo_info_container_div");
+      photoInfoContDiv.childNodes.forEach(currNode => {if(currNode.id == picId) photoInfoContDiv.removeChild(currNode);})
+    } else {
+      console.error("Error deleting photo");
+      alert("Unable to Delete the photo!");
+    }
+  })
+    .catch((error) => console.log("error", error));
+};
